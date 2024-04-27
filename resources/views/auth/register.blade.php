1bcfca6569e48282805e57d27212f99c50862347
Ravindu_Umayanga <74870496+RavinduISHA@users.noreply.github.com>
2024-04-27 09:36:08 +0530

    add rooms connections

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Register - SB Admin</title>
+        <title>Register - Admin</title>
         <link href="css/styles.css" rel="stylesheet" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
@@ -31,16 +31,15 @@
                                                     </div>
                                                     @error('f_name')
                                                     <div class="alert alert-danger">{{ $message }}</div>
-                                                @enderror
+                                                    @enderror
                                                 </div>
-                                               
                                                 <div class="col-md-6">
                                                     <div class="form-floating">
                                                         <input class="form-control" name="l_name" id="inputLastName" type="text" placeholder="Enter your last name" />
                                                         <label for="inputLastName">Last name</label>
                                                         @error('l_name')
                                                         <div class="alert alert-danger">{{ $message }}</div>
-                                                    @enderror
+                                                        @enderror
                                                     </div>
                                                 </div>
                                             </div>
@@ -59,7 +58,7 @@
                                                         <label for="inputPassword">Password</label>
                                                         @error('password')
                                                         <div class="alert alert-danger">{{ $message }}</div>
-                                                    @enderror
+                                                        @enderror
                                                     </div>
                                                    
                                                 </div>
@@ -69,7 +68,7 @@
                                                         <label for="inputPasswordConfirm">Confirm Password</label>
                                                         @error('confirm_password')
                                                         <div class="alert alert-danger">{{ $message }}</div>
-                                                    @enderror
+                                                        @enderror
                                                     </div>
                                                    
                                                 </div>
